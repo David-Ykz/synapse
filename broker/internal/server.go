7e64b7b2ce291067db95fc0bb23f5b7f0d701b43
David-Ykz <kzdavid314@gmail.com>
2026-02-08 00:02:01 -0500

    add broker

---
+++ broker/internal/server.go
@@ -0,0 +1,78 @@
+package broker
+
+import (
+	"fmt"
+	"net"
+	"synapse/common"
+)
+
+type Server struct {
+	Port int
+}
+
+func NewServer(port int) *Server {
+	return &Server{Port: port}
+}
+
+func (s *Server) handleConnection(conn net.Conn) {
+	defer conn.Close()
+	for {
+		packetType, namespace, data, err := common.ReadPacket(conn)
+		if err != nil {
+			fmt.Println("Error in reading packet:", err)
+			return
+		}
+		switch packetType {
+		case common.DISCONNECT:
+			return
+		case common.PRODUCER_MESSAGE:
+			fmt.Printf("Recived message from producer in namespace %s: %s\n", namespace, data)
+		case common.CONSUMER_MESSAGE:
+			if namespace == "namespace1" {
+				common.WritePacket(conn, common.SERVER_MESSAGE, namespace, "World Hello")
+			} else {
+				common.WritePacket(conn, common.SERVER_MESSAGE, namespace, "")
+			}
+		}
+	}
+}
+
+func (s *Server) Start() error {
+	broker := NewBroker(0, "namespace1", "/home/kzdavid/Documents/Github/synapse/temp", 4096)
+	err := broker.Initialize()
+	if err != nil {
+		return err
+	}
+
+	err = broker.Write("Message 1")
+	if err != nil {
+		return err
+	}
+
+	data, err := broker.ReadOne()
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(data))
+	broker.Write("Message 2")
+	broker.Write("Message 3")
+	data, _ = broker.ReadOne()
+	fmt.Println(string(data))
+
+	broker.Print()
+
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", s.Port))
+	if err != nil {
+		return err
+	}
+	fmt.Println("Broker listening on", s.Port)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection", err)
+			continue
+		}
+		go s.handleConnection(conn)
+	}
+}
